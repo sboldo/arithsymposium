329996160f12d7cfb2a4f7b19c500e781fcbfdcd
Sylvie Boldo <sylvie.boldo@inria.fr>
2026-05-26 16:22:43 +0200

    More files! (memoriam missing)

--- header.js
+++ header.js
@@ -1,9 +1,9 @@
 {
 		// all this just to factor out the common code.
 		header=`
-	<h1> IEEE International Symposium on Computer Arithmetic
+	<!--<h1> IEEE International Symposium on Computer Arithmetic
 	  <br>
-    <div style="font-size:2em; color:#FF0000;">ARITH</div>
+    <div style="font-size:2em; color:#FF0000;">ARITH</div>-->
     <img src="img/ARITH_logo.png" alt="ARITH logo">
 	</h1>
 `;
